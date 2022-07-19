efa02ebcfefef79592511bfa171a860980c647c4
Ubuntu <ubuntu@ip-172-31-26-209.ec2.internal>
2022-07-19 21:23:13 +0000

    updated contract version from solc0.4.17 to 0.8.3, also fixing compile, test and deploy code, missing minting process which will be added in a later stage

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -5,15 +5,46 @@ const fs = require('fs-extra');
 const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
 
-const campaignPath = path.resolve(__dirname, 'contracts', 'Repo.sol');
-const source = fs.readFileSync(campaignPath, 'utf8');
-const output = solc.compile(source, 1).contracts;
+const repoPath = path.resolve(__dirname, 'contracts', 'Repo.sol');
+const source = fs.readFileSync(repoPath, 'utf8');
+
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Repo.sol': {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*'],
+            },
+        },
+    },
+};
+
+//console.log(input); //verify that input is built correctly
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+//console.log(output); //verify that module.exports is built correctly
+
+const interface = output.contracts['Repo.sol'].Repo.abi;
+const bytecode = output.contracts['Repo.sol'].Repo.evm.bytecode.object;
+
+module.exports = {
+    interface,
+    bytecode
+}
+
+//module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Repo.sol'].Repo;
 
 fs.ensureDirSync(buildPath);
 
-for (let contract in output) {
+for (let contract in output.contracts['Repo.sol']) {
     fs.outputJsonSync(
         path.resolve(buildPath, contract.replace(':', '') + '.json'),
-        output[contract]
+        output.contracts['Repo.sol'][contract].abi
     );
 }
--- ethereum/contracts/Repo.sol
+++ ethereum/contracts/Repo.sol
@@ -1,84 +1,76 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.7;
-
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
-
+ 
+pragma solidity ^0.8.3;
+ 
 contract RepoFactory {
-    address[] public deployedRepos;
-
-    function createRepo(uint256 minimum) public {
-        address newRepo = new Repo(minimum, msg.sender);
-        deployedRepos.push(newRepo);
-    }
-
-    function getDeployedRepos() public view returns (address[] memory) {
-        return deployedRepos;
-    }
+   Repo[] public deployedRepos;
+ 
+   function createRepo(uint256 minimum) public  {
+       Repo repo = new Repo(minimum, msg.sender);
+       deployedRepos.push(repo);
+   }
+ 
+   function getDeployedRepos() public view returns (Repo[] memory) {
+      return deployedRepos;
+   }
 }
-
-contract Repo is NFTokenMetadata, Ownable {
-
-    struct AudioFile{
-        string description;
-        uint256 value;
-        string textTranscription;
-        address tokenAddress;
-    }
-
-    AudioFile[] public audioFiles; //Array for audio files 
-
-    address public manager;
-    uint256 public minimumContribution;
-    mapping(address => bool) public contributor; //    address[] public contributor;
-    uint256 public contributorsCount;
-
-    constructor(uint256 minimum, address creator) payable {
-        manager = creator;
-        minimumContribution = minimum;
-        nftName = "AudioFile";
-        nftSymbol = "AFL";
-    }
-
-    function contribute() public payable {
-    require(msg.value > minimumContribution);
-    contributor[msg.sender] = true; // contributor.push(msg.sender);
-    contributorsCount++;
-    }
-
-    function createAudioFile (
-        string memory _description, 
-        uint256 _value, 
-        string memory _textTranscription, 
-        address _tokenAddress) 
-    public {
-        AudioFile memory newAudioFile = AudioFile({
-            description:_description,
-            value:_value,
-            textTranscription:_textTranscription,
-            tokenAddress:_tokenAddress
-        });
-        audioFiles.push(newAudioFile);
-    }
-
-    function getSummary() public view returns (
-        uint256,
-        uint256,
-        uint256,
-        uint256,
-        address){
-        return (
-            minimumContribution,
-            address(this).balance,
-            audioFiles.length,
-            contributorsCount,
-            manager
-        );
-    }
-
-    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
-    super._mint(_to, _tokenId);
-    super._setTokenUri(_tokenId, _uri);
-  }
+ 
+contract Repo {
+ 
+   struct AudioFile{
+       string description;
+       uint256 value;
+       string textTranscription;
+       address tokenAddress;
+   }
+ 
+   AudioFile[] public audioFiles; //Array for audio files
+ 
+   address public manager;
+   uint256 public minimumContribution;
+   mapping(address => bool) public contributor; //    address[] public contributor;
+   uint256 public contributorsCount;
+ 
+   constructor(uint256 minimum, address creator) payable {
+       manager = creator;
+       minimumContribution = minimum;
+   }
+ 
+   function contribute() public payable {
+   require(msg.value > minimumContribution);
+   contributor[msg.sender] = true; // contributor.push(msg.sender);
+   contributorsCount++;
+   }
+ 
+   function createAudioFile (
+       string memory _description,
+       uint256 _value,
+       string memory _textTranscription,
+       address _tokenAddress)
+   public {
+       AudioFile memory newAudioFile = AudioFile({
+           description:_description,
+           value:_value,
+           textTranscription:_textTranscription,
+           tokenAddress:_tokenAddress
+       });
+       audioFiles.push(newAudioFile);
+   }
+ 
+   function getSummary() public view returns (
+       uint256,
+       uint256,
+       uint256,
+       uint256,
+       address){
+       return (
+           minimumContribution,
+           address(this).balance,
+           audioFiles.length,
+           contributorsCount,
+           manager
+       );
+   }
 }
+ 
+
